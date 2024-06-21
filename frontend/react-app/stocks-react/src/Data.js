42b2f5364d8cdfbfce14535c9c38a7ba16a03c42
Sven Radic <radic.sven@gmail.com>
2024-06-21 13:31:13 +0200

    added react app - crud for stocks

---
+++ frontend/react-app/stocks-react/src/Data.js
@@ -0,0 +1,27 @@
+export const Data = 
+  [
+    {
+      id: "3d5cc3d6-c35c-4c40-8ce8-ae5def279dc5",
+      symbol: "AAPL",
+      companyName: "APPLE",
+      currentPrice: 111,
+      marketCap: 123456,
+      traderId: "8cfd39d0-1893-4832-91da-6eb3bde09a61"
+    },
+    {
+      id: "a2c29fc3-7504-4462-8f93-7fa9d145e8de",
+      symbol: "MSFT",
+      companyName: "Microsoft",
+      currentPrice: 27,
+      marketCap: 98876161235,
+      traderId: "8cfd39d0-1893-4832-91da-6eb3bde09a61"
+    },
+    {
+      id: "41af0023-e9b8-47e8-b485-e7e251c98efa",
+      symbol: "TSLA",
+      companyName: "Tesla inc",
+      currentPrice: 150,
+      marketCap: 1242556,
+      traderId: "8cfd39d0-1893-4832-91da-6eb3bde09a61"
+    }
+  ];
